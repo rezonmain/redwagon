ec1f419a63b5ad1136d532ca9cc365a25b4d939f
rezonmain <adiazgomez101@gmail.com>
2020-04-25 01:01:20 -0700

    menu page done

--- js/menu-carousel.js
+++ js/menu-carousel.js
@@ -1,35 +1,48 @@
 const NUMBER_OF_PICTURES = 5;
 let currentPosition;
 
-
 let moveCarousel = function (direction) {
-  let getCarouselCell = document.querySelector(".carousel-cell");
-
   if (direction === "right") {
     currentPosition += 1;
-    if (currentPosition > NUMBER_OF_PICTURES) {
-      currentPosition = 1;
+    if (currentPosition > NUMBER_OF_PICTURES - 1) {
+      currentPosition = 0;
     }
   } else {
     currentPosition -= 1;
-    if (currentPosition == 0) {
-      currentPosition = NUMBER_OF_PICTURES;
+    if (currentPosition == -1) {
+      currentPosition = NUMBER_OF_PICTURES - 1;
     }
   }
+  let url;
   switch (currentPosition) {
+    case 0:
+      url = "img/menu1.jpg";
+      break;
     case 1:
-      getCarouselCell.style["background-image"] = 'url("./img/promo1.jpg")';
+      url = "img/menu2.jpg";
       break;
     case 2:
-      getCarouselCell.style["background-image"] = 'url("./img/promo2.jpg")';
+      url = "img/menu3.jpg";
       break;
     case 3:
-      getCarouselCell.style["background-image"] = 'url("./img/promo3.jpg")';
+      url = "img/menu4.jpg";
       break;
+    case 4:
+      url = "img/menu5.jpg";
   }
+  openCarousel(url, currentPosition);
 };
 
 let openCarousel = function (url, number) {
-  console.log(number)
-
+  let getMenuPageCard = document.querySelectorAll(".menu-page-card");
+  let getCarouselImage = document.querySelector(".carousel-image");
+  currentPosition = number;
+  getCarouselImage.style["background-image"] = "url(" + url + ")";
+  for (let i = 0; i < getMenuPageCard.length; i++) {
+    if (i == number) {
+      getMenuPageCard[i].style.opacity = "50%";
+    } else {
+      getMenuPageCard[i].style.opacity = "100%";
+    }
+  }
 };
---
+++ js/menu-reset.js
@@ -0,0 +1,99 @@
+let currentWidth;
+let getClickAway = document.querySelector(".clickAway");
+let getSidebar = document.querySelector(".side-nav");
+let getHamburgerBtn = document.querySelector(".animated-burger-btn");
+let getSidebarUl = document.querySelector(".nav-menu");
+let getDropIcon = document.querySelector(".drop-icon");
+let getMenuPageCard = document.querySelectorAll(".menu-page-card");
+
+let resetStyling = function (media) {
+  getSidebar.style.visibility = "hidden";
+  getSidebarUl.style.visibility = "hidden";
+  getClickAway.style.opacity = "0";
+  getHamburgerBtn.style.transform = "rotate(0deg)";
+  getClickAway.style.visibility = "hidden";
+  getDropIcon.style.transform = "rotate(0)";
+  toggleNavStatus = false;
+  if (media === "phone") {
+    getSidebar.style.height = "calc(100vh - var(--top-nav-height))";
+    getSidebar.style.width = "0";
+    for (let i = 0; i < getMenuPageCard.length; i++) {
+      getMenuPageCard[i].style.opacity = "100%";
+    }
+  } else if (media === "tablet") {
+    getSidebar.style.height = "0";
+    getSidebar.style.width = "100vw";
+    for (let i = 0; i < getMenuPageCard.length; i++) {
+      getMenuPageCard[i].style.opacity = "100%";
+    }
+  } else if (media === "desktop") {
+    getSidebar.style.visibility = "visible";
+    getSidebar.style.height = "var(--top-nav-height)";
+    getSidebar.style.width = "100vw";
+    getSidebarUl.style.visibility = "visible";
+    openCarousel("img/menu1.jpg", 0);
+  }
+};
+
+let hasViewPortChanged = function (currentWidth) {
+  //Ascending
+  if (previousWidth < 768 && currentWidth >= 768 && currentWidth < 1000) {
+    previousWidth = currentWidth;
+    return "phoneToTablet";
+  } else if (
+    previousWidth >= 768 &&
+    previousWidth < 1000 &&
+    currentWidth >= 1000
+  ) {
+    previousWidth = currentWidth;
+    return "tabletToDesktop";
+    //Descending
+  } else if (
+    previousWidth >= 1000 &&
+    currentWidth < 1000 &&
+    currentWidth >= 768
+  ) {
+    previousWidth = currentWidth;
+    return "desktopToTablet";
+  } else if (
+    previousWidth < 1000 &&
+    previousWidth >= 768 &&
+    currentWidth < 768
+  ) {
+    previousWidth = currentWidth;
+    return "tabletToPhone";
+    //Skipping
+  } else if (previousWidth < 768 && currentWidth >= 1000) {
+    previousWidth = currentWidth;
+    return "phoneToDesktop";
+  } else if (previousWidth >= 1000 && currentWidth < 768) {
+    previousWidth = currentWidth;
+    return "desktopToPhone";
+  } else {
+    return "unchanged";
+  }
+};
+let previousWidth = window.innerWidth;
+window.addEventListener("resize", function () {
+  getSidebar.style["transition-duration"] = "0s";
+  getClickAway.style["transition-duration"] = "0s";
+  currentWidth = window.innerWidth;
+  let change = hasViewPortChanged(currentWidth);
+  //console.log(change);
+  switch (change) {
+    case "phoneToTablet":
+    case "desktopToTablet":
+      resetStyling("tablet");
+      break;
+    case "tabletToDesktop":
+    case "phoneToDesktop":
+      resetStyling("desktop");
+      break;
+    case "tabletToPhone":
+    case "desktopToPhone":
+      resetStyling("phone");
+      break;
+  }
+});
+
+window.removeEventListener("resize", function () {});
